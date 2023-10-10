e2b0e53317fa0291696ddabbdbb84883056ffa6c
JanDomen <jandomenech@ceroca.cat>
2023-10-10 16:24:23 +0200

    ClaseGame

--- src/main/java/org/example/Game.java
+++ src/main/java/org/example/Game.java
@@ -19,6 +19,118 @@ public class Game implements Serializable {
     private String backlogs;
     private String wishlist;
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public ArrayList<String> getTeam() {
+        return team;
+    }
+
+    public void setTeam(ArrayList<String> team) {
+        this.team = team;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+
+    public int getTimesListed() {
+        return timesListed;
+    }
+
+    public void setTimesListed(int timesListed) {
+        this.timesListed = timesListed;
+    }
+
+    public int getNumberOfReviews() {
+        return numberOfReviews;
+    }
+
+    public void setNumberOfReviews(int numberOfReviews) {
+        this.numberOfReviews = numberOfReviews;
+    }
+
+    public ArrayList<String> getGenres() {
+        return genres;
+    }
+
+    public void setGenres(ArrayList<String> genres) {
+        this.genres = genres;
+    }
+
+    public String getSummary() {
+        return summary;
+    }
+
+    public void setSummary(String summary) {
+        this.summary = summary;
+    }
+
+    public ArrayList<String> getReviews() {
+        return reviews;
+    }
+
+    public void setReviews(ArrayList<String> reviews) {
+        this.reviews = reviews;
+    }
+
+    public String getPlays() {
+        return plays;
+    }
+
+    public void setPlays(String plays) {
+        this.plays = plays;
+    }
+
+    public String getPlaying() {
+        return playing;
+    }
+
+    public void setPlaying(String playing) {
+        this.playing = playing;
+    }
+
+    public String getBacklogs() {
+        return backlogs;
+    }
+
+    public void setBacklogs(String backlogs) {
+        this.backlogs = backlogs;
+    }
+
+    public String getWishlist() {
+        return wishlist;
+    }
+
+    public void setWishlist(String wishlist) {
+        this.wishlist = wishlist;
+    }
+
     public Game(int id, String title, String releaseDate, ArrayList<String> team, double rating, int timesListed,
                 int numberOfReviews, ArrayList<String> genres, String summary, ArrayList<String> reviews, String plays,
                 String playing, String backlogs, String wishlist) {
@@ -36,6 +148,7 @@ public class Game implements Serializable {
         this.playing = playing;
         this.backlogs = backlogs;
         this.wishlist = wishlist;
+
     }
 
 
